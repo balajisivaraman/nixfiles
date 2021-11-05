bf550b56190d820530f7ee7b86f5291137eb3c4a
Balaji Sivaraman <balaji@balajisivaraman.com>
2021-11-05 21:32:51 +0530

    fix: bs-emacs no longer exists

--- configuration.nix
+++ configuration.nix
@@ -4,7 +4,6 @@
     ./host/configuration.nix
     ./host/hardware.nix
     ./fonts/otf-san-francisco/configuration.nix
-    ./programs/bs-emacs.nix
     ./programs/i3lockr/configuration.nix
     ./services/change-wallpaper.nix
     ./services/nextcloud-client.nix
